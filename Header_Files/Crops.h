1ffffc117abb8dae3ea240e0f88fc195482b2f95
luisvoo-SIT <2503145@sit.singaporetech.edu.sg>
2026-02-18 09:42:58 +0000

    trying to get a rough idea on user input csv file to initialize multiple crops (objects)

--- Header_Files/Crops.h
+++ Header_Files/Crops.h
@@ -1,27 +1,52 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 using namespace std;
 
 class Crop {
 private:
-    int growthDaysMin,growthDaysMax;
-
-    
-    // Growth conditions
-    float minHumidity ,maxHumidity, minTemperature, maxTemperature, minUVIntensity, maxUVIntensity, waterRequirement;
-
+    string name;
+    int minHumidity;
+    int maxHumidity;
+    int minTemperature;
+    int maxTemperature;
+    int minUVIntensity;
+    int maxUVIntensity;
+    int waterRequirement;
+    int timeToGrow;
 
 public:
-    void Crop(string cropName, int minDays, int maxDays){
-        cropName = cropName;
-        growthDaysMin = minDays;
-        growthDaysMax = maxDays;
+    // Constructor
+    Crop(string n, int minH, int maxH, int minT, int maxT, int minUV, int maxUV, int water, int time) 
+        : name(n), minHumidity(minH), maxHumidity(maxH), 
+          minTemperature(minT), maxTemperature(maxT),
+          minUVIntensity(minUV), maxUVIntensity(maxUV),
+          waterRequirement(water), timeToGrow(time) {}
+
+    void display() const {
+        cout << "Crop: " << name << "\n"
+             << "Humidity: " << minHumidity << "-" << maxHumidity << "%\n"
+             << "Temperature: " << minTemperature << "-" << maxTemperature << "°C\n"
+             << "UV Intensity: " << minUVIntensity << "-" << maxUVIntensity << "\n"
+             << "Water Requirement: " << waterRequirement << "\n"
+             << "Time to Grow: " << timeToGrow << " months\n"
+             << "------------------------\n";
     }
-    void displayInfo()    {
-        cout << "Crop Name: " << cropName << endl;
-        cout << "Growth Days: " << growthDaysMin << " - " << growthDaysMax << endl;
-        cout << "Humidity Range: " << minHumidity << "% - " << maxHumidity << "%" << endl;
-        cout << "Temperature Range: " << minTemperature << "°C - " << maxTemperature << "°C" << endl;
-        cout << "UV Intensity Range: " << minUVIntensity << " - " << maxUVIntensity << endl;
-        cout << "Water Requirement: " << waterRequirement << " liters/day" << endl;
-    }
+    
+    string getName() const { return name; }
 };
+
+
+int main() {
+  // Create and open the csv file
+  string MyFileName;
+  cout << "Enter the name of the CSV file: ";
+  cin >> MyFileName;   
+  fstream MyFileName;
+
+
+
+
+
+}
+
