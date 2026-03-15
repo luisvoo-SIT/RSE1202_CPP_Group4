767c67848a50f2353ca16cc1c22e245ba3123b37
luisvoo-SIT <2503145@sit.singaporetech.edu.sg>
2026-03-15 15:15:05 +0800

    Update SprayerBot.h (CH)

--- Cpp_Files/Header_Files/SprayerBot.h
+++ Cpp_Files/Header_Files/SprayerBot.h
@@ -1,42 +1,40 @@
 #pragma once
 #include "Robots.h"
 #include <string>
-#include <vector>
 
 using namespace std;
 
 class SprayerBot : public Robot {
 public:
-    enum class SprayMode { FERTILIZER, PESTICIDE, HERBICIDE, CUSTOM };
+    enum class SprayMode { FERTILIZER, PESTICIDE, HERBICIDE };
 
 private:
-    SprayMode   mode;
-    double      tankCapacityL;
-    double      tankLevelL;
-    double      sprayRateL_per_m2;
-    string      chemicalName;
-    int         spraySessionsDone;
-    vector<int> scheduleDays;
+    SprayMode mode;
+    double    tankCapacityL;
+    double    tankLevelL;
+    double    sprayRateL_per_m2;
+    string    chemicalName;
+    int       spraySessionsDone;
 
     static string modeToString(SprayMode m);
 
 public:
     SprayerBot(const string& id,
-               SprayMode mode          = SprayMode::FERTILIZER,
-               double    tankCapacityL = 50.0,
-               double    sprayRate     =  0.3,
-               string    chemical      = "NPK-20-20-20");
+               SprayMode     mode,
+               double        tankCapacityL,
+               double        sprayRate,
+               string        chemicalName);
 
-    void setMode(SprayMode m)                { mode         = m;    }
-    void setChemical(const string& chem)     { chemicalName = chem; }
-    void setSchedule(const vector<int>& days){ scheduleDays = days; }
-    void refillTank();
+    void setMode(SprayMode m)         { mode         = m; }
+    void setChemical(const string& c) { chemicalName = c; }
 
     bool sprayArea(double areaSqM);
-    bool isScheduledToday(int dayOfWeek) const;
+    void refillTank();
+    void statusReport() const;
 
-    void performTask()        override;
-    void statusReport() const override;
-
-    double getTankLevel() const { return tankLevelL; }
+    SprayMode getMode()          const { return mode;             }
+    double    getTankLevel()     const { return tankLevelL;       }
+    double    getTankCapacity()  const { return tankCapacityL;    }
+    int       getSpraySessions() const { return spraySessionsDone;}
+    string    getChemicalName()  const { return chemicalName;     }
 };
