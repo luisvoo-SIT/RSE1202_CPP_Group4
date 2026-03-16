d1f64cd3738458d2368e5e359a483665d6980e4b
luisvoo-SIT <2503145@sit.singaporetech.edu.sg>
2026-03-16 07:58:50 +0000

    test time.h first

---
+++ Cpp_Files/Header_Files/time.h
@@ -0,0 +1,21 @@
+class Time {
+
+protected{
+    int plotTime
+    int GlobalTime
+}
+
+void timeSkip();
+int plotTime(int x,vector<vector<Plot>>& farm){
+    farm[r][c].time += x;
+
+    return x;
+
+
+
+};
+void GlobalTime(int x)
+void clear_plotTime(int x)
+void clear_GlobalTime(int x)
+
+}
