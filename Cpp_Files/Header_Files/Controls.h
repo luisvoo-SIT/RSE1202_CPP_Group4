7304c885e04710e20616c62bc31e3972afa56237
dinoygw <dinoyeogw@gmail.com>
2026-03-19 09:30:14 +0000

    Renamed header and commented on Controls.h

--- Cpp_Files/Header_Files/Controls.h
+++ Cpp_Files/Header_Files/Controls.h
@@ -1,26 +1,34 @@
 #ifndef CONTROLS_H
 #define CONTROLS_H
-#include "Actuators.h"
+#include "Header_Files/Actuators.h"
 #include <sstream>
 #include <limits>
 
+// Derived class: WaterSystemControl
 class WaterSystemControl : public Actuator {
 public:
+    // Function to adjust water level that takes in current water level as parameter
     void adjustWater(unsigned int &currentWater);
 };
 
+// Derived class: TempControl
 class TempControl : public Actuator {
 public:
+    // Function to adjust temperature that takes in current temperature level as parameter
     void adjustTemperature(unsigned int &currentTemp);
 };
 
+// Derived class: HumidityControl
 class HumidityControl : public Actuator {
 public:
+    // Function to adjust humidity that takes in current humidity level as parameter
     void adjustHumidity(unsigned int &currentHum);
 };
 
+// Derived class: UVControl
 class UVControl : public Actuator {
 public:
+    // Function to adjust UV level that takes in current UV level as parameter
     void adjustUV(unsigned int &currentUV);
 };
 
